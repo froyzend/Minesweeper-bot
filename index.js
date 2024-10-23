c1a771f9d7f3b386f39059afb01ace855da69322
Froyzend <froyzend1993@gmail.com>
2024-10-24 00:17:45 +0200

    upd10

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const token = "7700362550:AAHJv47-nEaHFJGvclx7qtFzCay0opMq7zI";
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
+  const chatId1 = msg.chat.id;
   bot.sendMessage(
-    chatId,
+    chatId1,
     "Добро пожаловать в игру Сапер! Нажмите /play для начала."
   );
 });
 
 bot.onText(/\/play/, (msg) => {
-  const chatId = msg.chat.id;
+  const chatId1 = msg.chat.id;
   const gameUrl = "https://minesweeper-bot-seven.vercel.app/"; // Ссылка на игру
-  bot.sendMessage(chatId, `Игра начинается! Вот ваша ссылка: ${gameUrl}`);
+  bot.sendMessage(chatId1, `Игра начинается! Вот ваша ссылка: ${gameUrl}`);
 });
