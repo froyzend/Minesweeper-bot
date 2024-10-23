6f4a2d0d938c858a7ba59fafdc95fdfa2f140d1b
Froyzend <froyzend1993@gmail.com>
2024-10-24 00:35:49 +0200

    upd12

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token1 = "7700362550:AAHJv47-nEaHFJGvclx7qtFzCay0opMq7zI";
-const bot = new TelegramBot(token1, { polling: true });
+const token = "7700362550:AAHJv47-nEaHFJGvclx7qtFzCay0opMq7zI";
+const bot = new TelegramBot(token, { polling: true });
 chatId2 = msg.chat.id;
 
 bot.onText(/\/start/, (msg) => {
-  const chatId1 = msg.chat.id;
+  const chatId = msg.chat.id;
   bot.sendMessage(
-    chatId1,
+    chatId,
     "Добро пожаловать в игру Сапер! Нажмите /play для начала."
   );
 });
 
 bot.onText(/\/play/, (msg) => {
-  const chatId1 = msg.chat.id;
+  const chatId = msg.chat.id;
   const gameUrl = "https://minesweeper-bot-seven.vercel.app/"; // Ссылка на игру
-  bot.sendMessage(chatId1, `Игра начинается! Вот ваша ссылка: ${gameUrl}`);
+  bot.sendMessage(chatId, `Игра начинается! Вот ваша ссылка: ${gameUrl}`);
 });
