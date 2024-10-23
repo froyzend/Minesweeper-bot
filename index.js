2fa1b2fdb327feb9a4c0449375c46142c50ba387
Froyzend <froyzend1993@gmail.com>
2024-10-24 00:54:42 +0200

    upd15

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
-const TelegramBot = require("node-telegram-bot-api");
+const { Telegraf } = require("telegraf");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const token = "7700362550:AAHJv47-nEaHFJGvclx7qtFzCay0opMq7zI";
-const bot = new TelegramBot(token, { polling: true });
-chatId2 = msg.chat.id;
+// Укажите свой токен, полученный от BotFather
+const bot = new Telegraf("7700362550:AAHJv47-nEaHFJGvclx7qtFzCay0opMq7zI");
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    "Добро пожаловать в игру Сапер! Нажмите /play для начала."
+// Обработка команды /start
+bot.start((ctx) => {
+  ctx.reply(
+    "Добро пожаловать в Minesweeper! Нажмите на кнопку ниже, чтобы начать игру.",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Начать игру",
+              url: "https://minesweeper-bot-seven.vercel.app/",
+            },
+          ],
+        ],
+      },
+    }
   );
 });
 
-bot.onText(/\/play/, (msg) => {
-  const chatId = msg.chat.id;
-  const gameUrl = "https://minesweeper-bot-seven.vercel.app/"; // Ссылка на игру
-  bot.sendMessage(chatId, `Игра начинается! Вот ваша ссылка: ${gameUrl}`);
+// Обработка команды /help
+bot.help((ctx) => {
+  ctx.reply(
+    'Это бот для игры в Minesweeper. Нажмите на кнопку "Начать игру", чтобы играть!'
+  );
+});
+
+// Обработка всех текстовых сообщений
+bot.on("text", (ctx) => {
+  ctx.reply("Пожалуйста, используйте кнопки для взаимодействия с ботом.");
+});
+
+// Запускаем сервер
+app.use(express.json());
+app.use(express.static("public")); // Отдача статических файлов из папки 'public'
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
+
+// Запускаем бота
+bot.launch().then(() => {
+  console.log("Бот запущен");
+});
+
+// Обработка ошибок
+process.once("SIGINT", () => {
+  bot.stop("SIGINT");
+});
+process.once("SIGTERM", () => {
+  bot.stop("SIGTERM");
 });
