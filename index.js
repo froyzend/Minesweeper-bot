1f2a9abd2c22e6bd576182e68468ae7a4b9988a6
Froyzend <froyzend1993@gmail.com>
2024-10-24 21:38:15 +0200

    newbot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 7700362550:AAHJv47-nEaHFJGvclx7qtFzCay0opMq7zIimport TelegramBot from "node-telegram-bot-api";mport { CBHandler, inlineMenu, Imenu } from "telegram-inline-menu";
 
-const TOKEN = "7700362550:AAHJv47-nEaHFJGvclx7qtFzCay0opMq7zI";
+const TOKEN = "7605809135:AAHN87WOl9ly7SZR7dYdTspohL3DY__IpjM";
 
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf(TOKEN);
